53bdc1f659bbf345e6b789e2b68255b42b8d8b95
Andrés Alegría <andresalegria@live.com>
2026-02-25 12:02:42 +0100

    Content update

--- src/config.js
+++ src/config.js
@@ -50,19 +50,6 @@ export default {
 
   chapters: [
 
-  // Chapter 0: Plain Text
-
-  // Chapter 1: September 2025
-
-  // Chapter 2: Video Footage
-
-  // Chapter 3: October 2025
-
-  // Chapter 4: November 2025
-
-  // Chapter 5: December 2025
-
-
 
 /////////////////////////////////////////////////////////////
 
@@ -83,7 +70,7 @@ export default {
 // },
 
   // Chapter 1: September 2025
-{
+  {
   id: "Chapter 1",
   alignment: 'left',
 
@@ -118,36 +105,107 @@ export default {
   ]
 },
 
+  // Chapter 2: Video Footage
+  {
+      id: 'chapter 2',
+      alignment: 'left',
+      hidden: false,
+      title: 'Video Footage',
+      description: "The ship appeared to survey a stretch of ocean just outside of the COMRA license area. Media accounts reported that researchers collected data, samples of seabed sediment and nodules, as well as deep-sea organisms and seawater. Footage from the expedition shows vast swathes of polymetallic nodules. The China Geological Survey did not respond to Mongabay’s request for more information.</b>",
+       
+      location: {
+        center: [-80.5, -20.5],
+        zoom: 5.25,
+        pitch: 0,
+        bearing: 0,
+      },
+      
+      mapAnimation: 'flyTo',
+      rotateAnimation: false,
+      onChapterEnter: [      ],
+      onChapterExit: [      ],
+    },
 
-  // Chapter X: Placeholder
+  // Chapter 3: October 2025
+  {
+      id: 'chapter 3',
+      alignment: 'left',
+      hidden: false,
+      title: 'October 2025',
+      description: "<p>In October — while the expedition was still reportedly taking place — the vessel traveled west through the Northern Mariana Islands, a U.S. commonwealth that serves as a vital military hub, before zigzagging up and down the border of its exclusive economic zone (EEZ) as well as just outside the EEZ of neighboring Guam, another U.S. territory.</p> <br> <p>Tom Shugart, a former U.S. Navy submarine warfare officer and expert in undersea warfare with a focus on the Indo-Pacific region, told Mongabay and CNN that he believed the vessel could have motives beyond deep-sea research.</p>",
+       
+      location: {
+        center: [-80.5, -20.5],
+        zoom: 5.25,
+        pitch: 0,
+        bearing: 0,
+      },
+      
+      mapAnimation: 'flyTo',
+      rotateAnimation: false,
+      onChapterEnter: [      ],
+      onChapterExit: [      ],
+    },
+    
+// Plain Text
 
 {
-  id: "Placeholder",
-  alignment: 'right',
+ id: "Chapter 0",
+ type: "stage",
+ stage: "PlainText",
+ title: "",
 
-  title: 'Placeholder',
-  description: "Placeholder Pathway",
+ html:
+`   <p>
+“That’s on the route that U.S. submarines might transit from Guam to places West,” said
+Shugart. He said he believed it was possible that the vessel could be leaving behind sensors at 4,600 meters below the ocean’s surface to record a submarine’s unique sound signature — the noise and vibrations a vessel makes in the water when it moves past. 
+</p>
+<p>
+“The sound signature of our submarines is a very, very valuable piece of information for them to get, so they know what they're looking for, what they're hunting for,” Shugart said. “Because if you don’t know what those frequencies are, then it's very difficult to pick a submarine out of a needle in the haystack -- out of all the other noise that’s in the ocean.”
+ </p> `
+},  
 
-  location: { center: [-158.067, -18.252], zoom: 4.2, pitch: 0, bearing: 0 },
-  
-  onChapterEnter: [
-    {
-      callback: "trackAnimation.start",
-      options: {
-      vesselFile: "/data/tracks/Da_Yang_Hao_track_Cook_Isl.geojson", 
-      speed: 1,
-      flyToStart: false,
-      }
-    }
+  // Chapter 4: November 2025
+  {
+      id: 'chapter 4',
+      alignment: 'left',
+      hidden: false,
+      title: 'November 2025',
+      description: "xxx.</b>",
+       
+      location: {
+        center: [-80.5, -20.5],
+        zoom: 5.25,
+        pitch: 0,
+        bearing: 0,
+      },
+      
+      mapAnimation: 'flyTo',
+      rotateAnimation: false,
+      onChapterEnter: [      ],
+      onChapterExit: [      ],
+    },
     
-  ],
-  onChapterExit: [
-    { callback: "trackAnimation.resume" },
-  ]
-},
-
-
-
+  // Chapter 5: December 2025
+  {
+      id: 'chapter 3',
+      alignment: 'left',
+      hidden: false,
+      title: 'December 2025',
+      description: "xxx.</b>",
+       
+      location: {
+        center: [-80.5, -20.5],
+        zoom: 3.25,
+        pitch: 0,
+        bearing: 0,
+      },
+      
+      mapAnimation: 'flyTo',
+      rotateAnimation: false,
+      onChapterEnter: [      ],
+      onChapterExit: [      ],
+    },
 
   ],
 };
